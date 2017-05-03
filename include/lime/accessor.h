1364ebde54fd11c537cafd86e402dec6718f629b
U-NEXUS\kil070 <Philip.Kilby@nicta.com.au>
2017-05-03 17:15:53 +1000

    Leet accessor work with named values

--- include/lime/accessor.h
+++ include/lime/accessor.h
@@ -1,37 +1,49 @@
 #pragma once
 
 #include <vector>
+#include <map>
 
 /**
- *  Defines a way of accessing values based on an enumuermaion of the fields
+ *  Defines a way of accessing values based on field names, or
+ *  an enumeration of fields
  */
 
 namespace lime
 {
-  class Accessor
-  {
-  public:
-
-    union ValueType
+    class Accessor
     {
-      long longVal;
-      unsigned long ulongVal;
-      double doubleVal;
+    public:
 
-      operator long&() {return longVal;}
-      operator unsigned long&() {return ulongVal;}
-      operator double&() {return doubleVal;}
+        union ValueType
+        {
+            long longVal;
+            unsigned long ulongVal;
+            double doubleVal;
+
+            operator long&() {return longVal;}
+            operator unsigned long&() {return ulongVal;}
+            operator double&() {return doubleVal;}
+        };
+
+        template<typename T>
+        T& get(const std::vector<ValueType>::size_type& field)
+        {
+            if (field >= val_.size())
+                val_.resize(field + 1);
+            return (T&) val_[field];
+        }
+
+        size_t indexFor (std::string name) {
+            auto iter = map_.find(name);
+            if (iter == map_.end()) {
+                map_[name] = map_.size();
+                return map_[name];
+            }
+            return iter->second;
+        }
+
+    private:
+        std::vector<ValueType> val_;
+        static std::map<std::string,size_t> map_;
     };
-
-    template<typename T>
-    T& get(const std::vector<ValueType>::size_type& field)
-    {
-      if (field >= val_.size())
-        val_.resize(field + 1);
-      return (T&) val_[field];
-    }
-
-  private:
-    std::vector<ValueType> val_;
-  };
 }
---
+++ src/accessor.cc
@@ -0,0 +1,4 @@
+
+#include "lime/accessor.h"
+
+std::map<std::string,size_t> lime::Accessor::map_;
