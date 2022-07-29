c6c54d7272b36939804e9a79f2c8e4101df9c4c7
Phil Kilby <philip.kilby@data61.csiro.au>
2022-07-29 12:42:45 +1000

    Add linereader

---
+++ include/lime/linereader.h
@@ -0,0 +1,73 @@
+#pragma once
+
+/* Use of this code is is subject to agreement to the
+   CSIRO Open Source Software Licence Agreement
+   as set out in the file ../../LICENCE.md
+*/
+
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <memory>
+#include <list>
+
+#include "lime/rand.h"
+#include "lime/limetok.h"
+
+namespace lime {
+
+    class LineReader
+    {
+    public:
+        LineReader(std::string filename);
+
+        /** Returns NULL at EOF */
+        bool getLine (
+            std::string& line, bool skipBlanks = true, bool skipComments = true
+        );
+
+        std::string currFilename() const {return currFrame_->filename_;}
+        int currLineNum() const {return currFrame_->lineNum_;}
+
+        // '#' automatically treated as comment delim
+        // Add another one
+        void addCommentString (std::string comment) {
+            comments_.push_back (comment);
+        }
+
+        // Calls limecrash
+        void error (std::string message);
+        
+    protected:
+        struct FileFrame
+        {
+            std::ifstream in_;
+            std::string filename_;
+            int lineNum_;
+
+            FileFrame (std::string filename) :
+                in_(filename),
+                filename_(filename),
+                lineNum_(0)
+            {
+            }
+            bool ok () const {return in_.good();}
+        };
+        using FileFramePtr = std::shared_ptr<FileFrame>;
+        using FileStack = std::list<FileFramePtr>;
+
+        // Does the string starting at k match a comment?
+        bool isComment (std::string str, int k);
+
+        /** Stack of open files (not including current) */
+        FileStack fileStack_;
+    
+        /** The current file frame */
+        FileFramePtr currFrame_;
+
+        std::list<std::string> comments_;
+    };
+
+} //namespace
+
---
+++ src/linereader.cc
@@ -0,0 +1,92 @@
+
+/* Use of this code is is subject to agreement to the
+   CSIRO Open Source Software Licence Agreement
+   as set out in the file ../LICENCE.md
+*/
+
+#include <cassert>
+#include <stdlib.h>
+
+#include "lime/debug.h"
+#include "lime/error.h"
+#include "lime/strutil.h"
+#include "lime/linereader.h"
+
+// Use space and tab as default delim
+constexpr const char* DELIM = " 	";
+
+using namespace std;
+using namespace lime;
+
+LineReader::LineReader (string filename) :
+    fileStack_(),
+    currFrame_(std::make_shared<FileFrame> (filename)),
+    comments_({ string("#") })
+{
+    DEBUG ('D', "Reading " << filename);
+    if (!currFrame_->ok())
+        limeCrash ("Couldn't open input file " << filename);
+}
+
+bool
+LineReader::getLine (string& line, bool skipBlanks, bool skipComments)
+{
+    while (true) { // Skip comments and (optionally) blanks
+        std::getline (currFrame_->in_, line);
+        if (!currFrame_->in_.good()) {
+            // Pop current file frame off stack
+            DEBUG ('D', "End of file");
+            if (fileStack_.empty()) {
+                return false;
+            }
+            currFrame_ = fileStack_.front();
+            fileStack_.pop_front();
+            DEBUG ('D', "Resume file " << currFrame_->filename_);
+            continue;
+        }
+        currFrame_->lineNum_++;
+        DEBUG ('D', currFrame_->lineNum_ << " >" << line << "<");
+        // find first non-blank
+        int k = 0;
+        while (k < line.length() && line[k] == ' ')
+            k++;
+        if (k < line.length() && line[k] == '@') {
+            // File redirection. Open a new file frame
+            fileStack_.push_front (currFrame_);
+            currFrame_ = make_shared<FileFrame> (line.substr(k+1));
+            if (!currFrame_->ok())
+                error (
+                    string ("Couldn't open redirected file ") +
+                    currFrame_->filename_
+                );
+            DEBUG (
+                'D', "Opened redirected file " << currFrame_->filename_
+            );
+            continue;
+        }
+        if (k == line.length() && !skipBlanks)
+            return true;
+        if (k < line.length() && (!skipComments || !isComment(line, k)))
+            return true;
+    }
+}
+
+bool
+LineReader::isComment (std::string str, int k)
+{
+    for (string cmnt : comments_) {
+        if (cmnt == str.substr (k, cmnt.length()))
+            return true;
+    }
+    return false;
+}
+
+
+void
+LineReader::error (string message)
+{
+    limeCrash (
+        message << " at line " << currFrame_->lineNum_ <<
+        " of " << currFrame_->filename_
+    );
+}
