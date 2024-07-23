080f666eac6175511b902ac2d4b5eacb8be7c84d
Phil Kilby <philip.kilby@data61.csiro.au>
2024-07-23 14:27:52 +1000

    Add limeAssert with description

--- include/lime/error.h
+++ include/lime/error.h
@@ -113,6 +113,9 @@ public:
     static void crash();
     static void progress ();
     static void _limeAssert(bool assertion, const char* file, int line);
+    static void _limeAssert(
+        std::string descr, bool assertion, const char* file, int line
+    );
     static void _limeAssert2(bool assertion, const char* file, int line);
 
 private:
@@ -132,4 +135,5 @@ private:
 #define limeProgress(X) {LimeError::errorStream() << X; LimeError::progress();}
 
 #define limeAssert(X) {LimeError::_limeAssert((X),__FILE__,__LINE__);}
+#define limeAssertDesc(DESC,X) {LimeError::_limeAssert((DESC),(X),__FILE__,__LINE__);}
 #define limeAssert2(X, Y) {LimeError::errorStream() << Y; LimeError::_limeAssert2((X), __FILE__,__LINE__);}
